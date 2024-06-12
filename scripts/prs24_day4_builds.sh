1b35b99029be36f9ae87fcfc91ff23261f6f8f7c
j-darocha <jdarocha1@gmail.com>
2024-06-12 10:57:47 +0100

    Create prs24_day4_builds.sh

---
+++ scripts/prs24_day4_builds.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## List of dependencies for Day 4
+## Installing cmake
+sudo apt-get install build-essential libssl-dev
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
+gunzip  Downloads/cmake-3.29.4.tar.gz 
+sudo apt-get -y install cmake
+
+##R packages
+SCRIPT_FILE="rscript_packages.R"
+
+echo "" > $SCRIPT_FILE
+Install.packages(“Rcpp”) # Required package before “reshape2 package” installation
+Install.packages(“reshape2”)
+install.packages(“viridisLite”)# Required before installing “viridis”
+install.packages(“viridis”)
+install Bioconduct # Required package before “GenomicRanges” package installation
+install.packages(“GenomicRanges”)
+install.packages(“dplyr”)
+install.packages(“tidyr”)
+install.packages(“vcfR”)
+install.packages(“memuse”)
+install.packages(“panelr”)
+install.packages(“data.table”)
+install.packages(“panelr”)
+install.packages(“panelr”)
+install.packages(“panelr”)
+install.packages(“panelr”) >> $SCRIPT_FILE
+
+#run R install
+R rscript_packages.R
+
+#intalling bcf tools
+
+sudo apt-get install autoconf automake make gcc perl zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libssl-dev libncurses5-dev
+wget https://github.com/samtools/samtools/releases/download/1.3.1/samtools-1.3.1.tar.bz2 -O samtools.tar.bz2
+tar -xjvf samtools.tar.bz2
+cd samtools-{version}
+cd samtools-1.3.1/
+make
+sudo make prefix=/usr/local/bin install
+wget https://github.com/samtools/bcftools/releases/download/1.3.1/bcftools-1.3.1.tar.bz2 -O bcftools.tar.bz2
+tar -xjvf bcftools.tar.bz2
+cd bcftools-1.3.1/
+make
+sudo make prefix=/usr/local/bin install
+sudo ln -s /usr/local/bin/bin/bcftools /usr/bin/bcftools
+
